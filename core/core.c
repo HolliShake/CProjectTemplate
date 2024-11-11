fa006e57b154452f0c9c3bf1f5f4f3cb507f581d
Philipp Andrew Redondo <124238055+HolliShake@users.noreply.github.com>
2024-11-11 22:28:51 +0800

    UPDATE: added GC_gcollect() at core.c when program is finished

--- core/core.c
+++ core/core.c
@@ -4,5 +4,6 @@ int main(int argc, char* argv[]) {
     GC_INIT();
     int status = start(argc, argv);
     printf("Program exited with status %d\n", status);
+    GC_gcollect();
     return status;
 }
